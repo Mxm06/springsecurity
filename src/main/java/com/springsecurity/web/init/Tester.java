2688a4d6c66e3657ebdfeb86fba83b03245abc46
Mxm06 <stingermax06@mail.ru>
2024-03-21 14:21:04 +0300

    Added tester

---
+++ src/main/java/com/springsecurity/web/init/Tester.java
@@ -0,0 +1,18 @@
+package com.springsecurity.web.init;
+
+public class Tester {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
